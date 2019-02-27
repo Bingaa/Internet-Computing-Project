84abbd3f2dc6a9ec06039ade674e5270322d7000
Bingaa <jasmaanp@gmail.com>
2019-02-27 16:42:57 -0500

    Added modal when profile pic clicked

--- scripts/profile.js
+++ scripts/profile.js
@@ -12,3 +12,27 @@ function readURL(f) {
         read.readAsDataURL(f.files[0]);
     }
 }
+
+window.onload = function() { 
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("profile");
+    var span = document.getElementsByClassName("close")[0];
+
+
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+}
