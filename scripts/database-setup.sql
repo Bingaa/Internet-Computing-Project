c6895464fe04056ad1956ffe2591c3a05f683162
ayang0302 <yang6730@mylaurier.ca>
2019-03-30 16:17:08 -0400

    create messages and usergroup tables

--- scripts/database-setup.sql
+++ scripts/database-setup.sql
@@ -13,7 +13,8 @@ CREATE TABLE `User` (
   `UserName` VARCHAR(50) NOT NULL, 
   `Password` VARCHAR(50) NOT NULL,
   `FirstName` VARCHAR(50) NOT NULL, 
-  `LastName` VARCHAR(50) NOT NULL, 
+  `LastName` VARCHAR(50) NOT NULL,
+  `Image` VARCHAR(50), 
   `Job` VARCHAR(50), 
   `Location` VARCHAR(50), 
   `Email` VARCHAR(50) NOT NULL,  
@@ -25,3 +26,37 @@ CREATE TABLE `User` (
 
 INSERT INTO User (`UserName`, `Password` , `FirstName` , `LastName` , `Email`)
 VALUES ('andrew69', 'andrew69', 'Andrew', 'Fong', 'fong7680@mylaurier.ca')
+
+
+#
+# Table structure for table 'User'
+#
+
+DROP TABLE IF EXISTS `UserGroup`;
+
+CREATE TABLE `UserGroup` (
+  `GroupID` INTEGER NOT NULL AUTO_INCREMENT,
+  'Usernames' VARCHAR(50) NOT NULL UNIQUE, 
+  `JoinDate` DATETIME, 
+  PRIMARY KEY (`GroupID`)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+
+SET autocommit=1;
+
+#
+# Table structure for table 'User'
+#
+
+DROP TABLE IF EXISTS `Message`;
+
+CREATE TABLE `Message` (
+  `MessageID` INTEGER NOT NULL AUTO_INCREMENT, 
+  'Type' VARCHAR(50) NOT NULL, 
+  'Content' LONGTEXT NOT NULL, 
+  `CreateDate` DATETIME,  
+  `SenderID` INTEGER NOT NULL AUTO_INCREMENT,
+  `ReceiverID` INTEGER NOT NULL AUTO_INCREMENT, 
+  PRIMARY KEY (`MessageID`)
+) ENGINE=innodb DEFAULT CHARSET=utf8;
+
+SET autocommit=1;
