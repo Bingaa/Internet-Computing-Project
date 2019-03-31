75ed2c91d4ea9e625aaf15b37a65184bb8828f0b
Andrew F <me@andrewfong.ca>
2019-03-30 22:21:32 -0400

    added enter key functionality for forgotpassword.html

--- scripts/passwordajax.js
+++ scripts/passwordajax.js
@@ -1,10 +1,11 @@
 
 $(document).ready(function(){
     $("#forgot").click(function(){
+
       var request = $.ajax({
         type:"GET",
         url: "../scripts/passwordReset.php",
-        data:{input:"andrew69"}, //$("#info").value
+        data:{input:$("#info").val()},
         dataType:'text',
       });
       request.done(function(result){
@@ -15,8 +16,26 @@ $(document).ready(function(){
         appended="We were unable to find your information in the database.  Check if you mispelled something!";
         $("#blurb").html(textStatus);
       });
-  
-  
-  
     });
+
+    $('#info').keypress(function(event){
+      var keycode = (event.keyCode ? event.keyCode : event.which);
+      if(keycode == '13'){
+
+        var request = $.ajax({
+          type:"GET",
+          url: "../scripts/passwordReset.php",
+          data:{input:$("#info").val()},
+          dataType:'text',
+        });
+        request.done(function(result){
+          appended="We sent you an email to "+result+".  Check it out!";
+          $("#blurb").html(appended);
+        });
+        request.fail(function(jqXHR,textStatus){
+          appended="We were unable to find your information in the database.  Check if you mispelled something!";
+          $("#blurb").html(textStatus);
+        });
+      }
+  });
 });
