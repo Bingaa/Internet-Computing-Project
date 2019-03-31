be53d9e914ae7fd959a501a281230ad2b63140a6
Bingaa <jasmaanp@gmail.com>
2019-03-30 21:34:59 -0400

    rename index.html -> index.php

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 
 <head> 
         <link rel="stylesheet" type="text/css" href="styles/styles.css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="scripts/signIn.js"></script>
+
 </head>
 
 <body> 
     <div class="white-card" style="margin-top: 100px;">
         <img src= "images/Logo.PNG">
-        <form action="html/messages.html"> 
+        <form action="index.php" method="post"> 
             <input type="text" name="userName" placeholder="Username">
             <input type="password" name="password" placeholder="Password">
             <button type="submit" value="Login">Login</button>
