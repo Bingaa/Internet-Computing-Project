68c31116cb905333fe51e8f9ae357887a0f24b3d
Andrew F <me@andrewfong.ca>
2019-04-03 16:15:21 -0400

    Fixed password reset feature

--- scripts/passwordReset.php
+++ scripts/passwordReset.php
@@ -8,7 +8,14 @@ $parameter = $_GET['input'];
 $q = "SELECT * FROM user WHERE Email='".$parameter."' OR UserName='".$parameter."' LIMIT 1;"; 
 $data=runQuery($chattime,$q);
 $r = mysqli_fetch_assoc($data);
-mail($r['Email'],"Password Information", "Your password is ".$r['Password']." and your username is ".$r['UserName'].".");
+$n = rand(1000,9999);
+$newhash = password_hash('forget'.$n,PASSWORD_DEFAULT);
+
+$q2 = "UPDATE `user` SET `Password` ='".$newhash."' WHERE Email='".$parameter."' OR UserName='".$parameter."' LIMIT 1;";
+$data2 = runQuery($chattime,$q2);
+
+
+mail($r['Email'],"Password Information", "Your password is forget".$n." and your username is ".$r['UserName'].".".$newhash);
 echo $r['Email'];
 
 
