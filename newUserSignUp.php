009f2656e66a6ccfbdc1e7cc87d2957d8bb66a6d
Andrew F <me@andrewfong.ca>
2019-04-04 01:41:53 -0400

    newusersignup page binding parameters

--- newUserSignUp.php
+++ newUserSignUp.php
@@ -11,17 +11,56 @@ $email = $_REQUEST["email"];
 
 $hashed_password = password_hash($password,PASSWORD_DEFAULT);
 
-$query = "INSERT INTO User (`UserName`, `Password` , `FirstName` , `LastName` , `Email`)
-             VALUES ('" .$username."','" .$hashed_password."','" .$firstName."','" .$lastName."','" .$email . "')"; 
-$results = runQuery($connection, $query);
+
+$querybuilder = "INSERT INTO User (`UserName`, `Password` , `FirstName` , `LastName` , `Email`) VALUES (?,?,?,?,?)";
+$stmt = mysqli_stmt_init($connection);
+if (!mysqli_stmt_prepare($stmt,$querybuilder)){
+    echo "SQL Statement failure";
+}else{
+    mysqli_stmt_bind_param($stmt,"sssss", $username, $hashed_password, $firstName, $lastName, $email);
+    //run param inside database
+    mysqli_stmt_execute($stmt);
+    $results = mysqli_stmt_get_result($stmt);
+}
+
+
+
+// $query = "INSERT INTO User (`UserName`, `Password` , `FirstName` , `LastName` , `Email`)
+//              VALUES ('" .$username."','" .$hashed_password."','" .$firstName."','" .$lastName."','" .$email . "')"; 
+// $results = runQuery($connection, $query);
+
+
+
+
+
+
 if($results != false){ 
-    $query = "SELECT UserID FROM User WHERE UserName = '" .$username ."'"; 
-    $results = runQuery($connection, $query);  
-    $id = mysqli_fetch_assoc($results)["UserID"];      
-    session_start(); 
-    $_SESSION["id"] = $id; 
-    $_SESSION["fullName"] = $firstName . " " . $lastName; 
-    echo "true"; 
+
+
+    $querybuilder = "SELECT UserID FROM User WHERE UserName = ?";
+    $stmt = mysqli_stmt_init($connection);
+    if (!mysqli_stmt_prepare($stmt,$querybuilder)){
+        echo "SQL Statement failure";
+    }else{
+        mysqli_stmt_bind_param($stmt,"s", $username);
+        //run param inside database
+        mysqli_stmt_execute($stmt);
+        $results = mysqli_stmt_get_result($stmt);
+        $id = mysqli_fetch_assoc($results)["UserID"];      
+        session_start(); 
+        $_SESSION["id"] = $id; 
+        $_SESSION["fullName"] = $firstName . " " . $lastName; 
+        echo "true"; 
+    }
+
+    // $query = "SELECT UserID FROM User WHERE UserName = '" .$username ."'"; 
+    // $results = runQuery($connection, $query);  
+
+
+
+
+
+
 } else { 
     echo "false"; 
 }
